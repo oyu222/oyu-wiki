22d7725836a0c8e07e4c814c50358122d91f4423
oyu222 <32892625+oyu222@users.noreply.github.com>
2026-09-13 22:56:20 +0900

    Create supabase.js

---
+++ supabase.js
@@ -0,0 +1,7 @@
+const SUPABASE_URL = "https://sinnuqbrxotapgedufjs.supabase.co";
+const SUPABASE_PUBLISHABLE_KEY = "sb_publishable_Fay1J4-4Wue4Ln7muJStrw_B_Wi8V86";
+
+const supabaseClient = window.supabase.createClient(
+    SUPABASE_URL,
+    SUPABASE_PUBLISHABLE_KEY
+);
