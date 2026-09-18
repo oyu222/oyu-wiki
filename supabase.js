a8be52312e3a456087ba2c8d7581ea390e8bdf94
oyu222 <32892625+oyu222@users.noreply.github.com>
2026-09-18 22:15:55 +0900

    Update supabase.js

--- supabase.js
+++ supabase.js
@@ -1,7 +1,6 @@
-const SUPABASE_URL = "https://sinnuqbrxotapgedufjs.supabase.co";
-const SUPABASE_PUBLISHABLE_KEY = "sb_publishable_Fay1J4-4Wue4Ln7muJStrw_B_Wi8V86";
-
-const supabaseClient = window.supabase.createClient(
-    SUPABASE_URL,
-    SUPABASE_PUBLISHABLE_KEY
-);
+window.supabaseClient =
+    window.supabaseClient ||
+    window.supabase.createClient(
+        "https://sinnuqbrxotapgedufjs.supabase.co",
+        "sb_publishable_Fay1J4-4Wue4Ln7muJStrw_B_Wi8V86"
+    );
